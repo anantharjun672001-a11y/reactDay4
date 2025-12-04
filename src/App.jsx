277f487fa4edb3cd5124b4c1859618fdfb822981
anantharjun672001-a11y <anantharjun672001@gmail.com>
2025-12-04 09:48:47 +0530

    cart add,remove completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import CompA from './Components/CompA';
 
 const App = () => {
   const [value,setValue]=useState(10)
@@ -13,6 +14,7 @@ const App = () => {
       <h1>React</h1>
       <p>{value}</p>
       <button onClick={handleSubmit}>Add</button>
+      <CompA/>
     </div>
     
   );
---
+++ src/Components/CompA.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+
+const CompA = () => {
+    const[item,setItem]=useState(0);
+   /*  const[status, setStatus] = useState(true); */
+
+    const handleInc=()=>{
+        /* setStatus(false); */
+        setItem((item) => item + 1);
+    };
+
+    const handleDec=()=>{
+        /* setStatus(true); */
+        if (item > 0) {
+        setItem((item) => item - 1);
+        }
+    };
+    
+
+    return (
+        <div>
+            <h1>Cart Component</h1>
+            <h1>Cart Value: {item}</h1>
+            {/* {status ? (
+                <button onClick={handleInc}>Add To Cart</button>
+            ) : (
+                <button onClick={handleDec}>Remove From Cart</button>
+            )} */}
+            <button onClick={handleInc} value='add'>+</button>
+            <button onClick={handleDec}value='decrease'>-</button>
+        </div>
+    );
+};
+
+export default CompA;
